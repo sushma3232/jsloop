dbdee9b5b8a8b53d35570d15750fdd4653af933b
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-02-10 10:40:24 +0530

    Add files via upload

---
+++ 1stqu.js
@@ -0,0 +1,13 @@
+// var a="sushma";
+// var b=4;
+// console.log(a*b);
+// console.log(a+a+a+a)
+
+var name="deepthi";
+var num="7";
+var num1=7.7;
+var num2=Number(num)
+var x=parseInt(num1)
+
+
+console.log("pooja"+name,"=",num2+x)
